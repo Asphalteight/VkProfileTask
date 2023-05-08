729dd67e649e55cb8f0eac2dbb79d4de7c63493b
Nikolay <nikolaynekr.17@gmail.com>
2023-05-08 17:28:05 +0300

    Начальная структура проекта, добавлены доменные модели

---
+++ src/Task/Domain/Task.Domain/Users/UserState.cs
@@ -0,0 +1,27 @@
+﻿namespace Task.Domain.Users;
+
+/// <summary>
+/// Модель стутусов пользователей.
+/// </summary>
+public class UserState
+{
+    /// <summary>
+    /// Идентификатор.
+    /// </summary>
+    public int Id { get; set; }
+    
+    /// <summary>
+    /// Название статуса.
+    /// </summary>
+    public string Code { get; set; } = null!;
+    
+    /// <summary>
+    /// Описание.
+    /// </summary>
+    public string Description { get; set; } = null!;
+    
+    /// <summary>
+    /// Список пользователей.
+    /// </summary>
+    public virtual List<User>? UsersList { get; set; }
+}
